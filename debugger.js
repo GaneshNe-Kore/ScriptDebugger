6e7800249914bfcd0debeef9f689f5ed949706f5
KI0981\Ganesh.Nethula <ganesh.nethula@kore.com>
2024-02-11 00:52:33 +0530

    adding the debugger log script

--- debugger.js
+++ debugger.js
@@ -1,5 +1,6 @@
-const getBotVariables = require('./hiddenFiles/getBotVariables');
-// PLease make sure to download the latest env and content variable before starting debugging.
-// getBotVariables()
 const script = require('./script');
-script.debug();
+const getBotVariables = require('./hiddenFiles/getBotVariables');
+
+// PLease make sure to download the latest env and content variable before starting debugging.
+getBotVariables()
+// script.debug();
